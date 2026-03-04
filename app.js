1c0352a614a185e05c3ad6f22c1d2f41413827f5
richsemic13 <joca.semic.ui@phinmaed.com>
2026-03-05 04:03:35 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+function showRoute() {
+  const route = document.getElementById("routeSelect").value;
+  const output = document.getElementById("output");
+
+  if (route === "") {
+    output.innerHTML = "Please select a route.";
+  }
+  else if (route === "Jaro - City Proper") {
+    output.innerHTML = "Fare: ₱13 | Estimated Time: 15 mins";
+  }
+  else if (route === "Mandurriao - SM") {
+    output.innerHTML = "Fare: ₱15 | Estimated Time: 20 mins";
+  }
+  else if (route === "La Paz - CPU") {
+    output.innerHTML = "Fare: ₱12 | Estimated Time: 10 mins";
+  }
+}
+
+/* REGISTER SERVICE WORKER */
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("service-worker.js")
+    .then(() => console.log("Service Worker Registered"));
+}
---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "iloilo-route-cache-v2";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "index.html",
+        "style.css",
+        "app.js"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
